b5d4ab9b39fe7c07095924a3cf3361521b85a43c
Khess S Dotto <khessdotto@gmail.com>
2023-01-13 18:03:08 -0300

    Done the first 4 iterations. Missing iterations 5 and 6 (and bonuses).

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,74 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  return moviesArray.map((x) => x.director);
+};
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  let count = 0;
+  for (let i = 0; i < moviesArray.length; i++) {
+    if (moviesArray[i].director.toUpperCase() === 'STEVEN SPIELBERG' && (moviesArray[i].genre.includes('DRAMA') ||
+                                                                         moviesArray[i].genre.includes('drama') ||
+                                                                         moviesArray[i].genre.includes('Drama'))) {
+      count++;
+    };
+  };
+  return count;
+};
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (moviesArray.length === 0) {
+    return 0;
+  };
+
+  let sum = 0;
+  let count = 0;
+
+  for (let i = 0; i < moviesArray.length; i++) {
+    if (moviesArray[i].score !== undefined && moviesArray[i].score !== null) {
+      sum += moviesArray[i].score;
+      count++;
+    }else{
+      count++;
+    };
+  };
+  const average = Math.round(sum / count * 100) / 100;
+  return average;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  if (moviesArray.length === 0) {
+    return 0;
+  };
+    
+  let sum = 0;
+  let count = 0;
+    
+  for (let i = 0; i < moviesArray.length; i++) {
+    if (moviesArray[i].score !== undefined && moviesArray[i].score !== null && (moviesArray[i].genre.includes('DRAMA') ||
+                                                                                moviesArray[i].genre.includes('drama') ||
+                                                                                moviesArray[i].genre.includes('Drama'))) {
+      sum += moviesArray[i].score;
+      count++;
+    }else if (moviesArray[i].genre.includes('DRAMA') ||
+              moviesArray[i].genre.includes('drama') ||
+              moviesArray[i].genre.includes('Drama')) {
+      count++;
+    };
+  };
+
+  if (count === 0) {
+    return 0;
+  };
+
+  const average = Math.round(sum / count * 100) / 100;
+  return average;
+};
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
